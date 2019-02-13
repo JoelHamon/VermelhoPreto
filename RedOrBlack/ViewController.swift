5f4788614f74d782e1bc4bcfabeae79f2931f44e
JoelHamon <joelmhamon@gmail.com>
2019-02-13 14:24:12 -0400

    Initial Commit

---
+++ RedOrBlack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedOrBlack
+//
+//  Created by Joel Menezes Hamon on 13/02/19.
+//  Copyright © 2019 Joel Menezes Hamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
